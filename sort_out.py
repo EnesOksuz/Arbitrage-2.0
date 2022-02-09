0842cba82ce3d34bd3c15b1e8d7099be7202afde
Enes ÖKSÜZ <oksuzenes78@gmail.com>
2022-02-09 14:44:56 +0300

    I merged two functions and make it faster than 10x :D. now less code ,more faster, and selected more useful coins

--- sort_out.py
+++ sort_out.py
@@ -1,95 +1,72 @@
-import re
 import requests
+import time
 def getCoin():
     coins2=[]
-    kucoin_cur = "https://api.kucoin.com/api/v1/currencies"
-    huobi_cur = "https://api.huobi.pro/v1/common/symbols"
-    okex_cur ="https://www.okex.com/api/spot/v3/instruments"
-    mexc_cur ="https://www.mexc.com/open/api/v2/market/symbols"
-    bybit_cur ="https://api.bybit.com/v2/public/symbols"
-    res_kucoin_cur= requests.get(kucoin_cur).json()
-    res_huobi_cur= requests.get(huobi_cur).json()
-    res_okex_cur= requests.get(okex_cur).json()
-    res_mexc_cur = requests.get(mexc_cur).json()
-    res_bybit_cur =requests.get(bybit_cur).json()
-    for x in range(len(res_kucoin_cur["data"])):
-        coins2.append(res_kucoin_cur["data"][x]["currency"])
-    for x in range(len(res_huobi_cur["data"])):
-        coins2.append(res_huobi_cur["data"][x]["base-currency"].upper())
-    for x in range(len(res_okex_cur)):
-        coins2.append(res_okex_cur[x]["base_currency"])
-    for x in range(len(res_mexc_cur["data"])):
-        coins2.append(res_mexc_cur["data"][x]["vcoinName"])
-    for x in range (len(res_bybit_cur["result"])):
-        coins2.append(res_bybit_cur["result"][x]["base_currency"])
+    sortedcoins=[]
+    kucoin_cur = requests.get("https://api.kucoin.com/api/v1/currencies")
+    kucoin_coins=[]
+    for y in range(len(kucoin_cur.json()["data"])):
+        kucoin_coins.append(kucoin_cur.json()["data"][y]["currency"])
+    
+    huobi_cur = requests.get("https://api.huobi.pro/v1/common/currencys")
+    huobi_coins = huobi_cur.json()["data"]
+    
+    okex_cur =requests.get("https://www.okex.com/api/spot/v3/instruments")
+    __okex_coins=[]
+    for y in range(len(okex_cur.json())):
+        __okex_coins.append(okex_cur.json()[y]["base_currency"])
+    okex_coins = list(dict.fromkeys(__okex_coins))
+
+    mexc_cur =requests.get("https://www.mexc.com/open/api/v2/market/symbols")
+    __mexc_coins=[]
+    for y in range(len(mexc_cur.json()["data"])):
+        __mexc_coins.append(mexc_cur.json()["data"][y]["vcoinName"])
+    mexc_coins= list(dict.fromkeys(__mexc_coins))
+
+    ftx_cur = requests.get("https://ftx.com/api/wallet/coins")
+    ftx_coins=[]
+    for y in range(len(ftx_cur.json()["result"])):
+        ftx_coins.append(ftx_cur.json()["result"][y]["id"])
+
+    bybit_cur =requests.get("https://api.bybit.com/v2/public/symbols")
+    bybit_coins=[]
+    for y in range(len(bybit_cur.json()["result"])):
+        bybit_coins.append(bybit_cur.json()["result"][y]["base_currency"])
+
+    coins2= kucoin_coins+ huobi_coins+okex_coins+ mexc_coins+ ftx_coins + bybit_coins
+
     coins=list(dict.fromkeys(coins2))
-    #coins.sort()
-    return coins
-
-
-def sort(coins):
     for x in range(len(coins)):
         print(coins[x])
-        counter =0
-        kucoin_control  = requests.get(f"https://api.kucoin.com/api/v1/currencies")
-        huobi_control   = requests.get("https://api.huobi.pro/v1/common/currencys")
-        okex_control    = requests.get("https://www.okex.com/api/spot/v3/instruments")
-        mexc_control    = requests.get("https://www.mexc.com/open/api/v2/market/symbols")
-        ftx_control     = requests.get("https://ftx.com/api/wallet/coins")
-        bybit_control   = requests.get("https://api.bybit.com/v2/public/symbols")
-        
-        
-    #-------------------FOR KUCOİN---------------------------------------------
-        kucoin_coins =[]
-        for y in range(len(kucoin_control.json()["data"])):
-            kucoin_coins.append(kucoin_control.json()["data"][y]["currency"])
+        counter=0
         for y in range(len(kucoin_coins)):
             if(coins[x]==kucoin_coins[y]):
                 counter+=1
-        # if(kucoin_control.json()["code"]=="200000"):
-        #     counter+=1
-    #-----------------FOR HUOBI--------------------------------------------------------
-        huobi_coins = huobi_control.json()["data"]
         for y in range(len(huobi_coins)):
             if(coins[x]==huobi_coins[y].upper()):
                 counter+=1
-    #-----------------FOR OKEX--------------------------------------------------------
-        __okex_coins=[]
-        for y in range(len(okex_control.json())):
-            __okex_coins.append(okex_control.json()[y]["base_currency"])
-        okex_coins = list(dict.fromkeys(__okex_coins))
-        
         for y in range(len(okex_coins)):
             if(coins[x]==okex_coins[y]):
                 counter+=1
-     #-----------------FOR MEXC--------------------------------------------------------
-        __mexc_coins=[]
-        for y in range(len(mexc_control.json()["data"])):
-            __mexc_coins.append(mexc_control.json()["data"][y]["vcoinName"])
-        mexc_coins= list(dict.fromkeys(__mexc_coins))
         for y in range(len(mexc_coins)):
             if(coins[x]==mexc_coins[y]):
                 counter+=1
-    #--------------------- FOR FTX---------------------------------------------
-        ftx_coins=[]
-        for y in range(len(ftx_control.json()["result"])):
-            ftx_coins.append(ftx_control.json()["result"][y]["id"])
         for y in range(len(ftx_coins)):
             if(coins[x]==ftx_coins[y]):
                 counter+=1
-    #------------------ for bybit------------------------------------------
-        bybit_coins=[]
-        for y in range(len(bybit_control.json()["result"])):
-            bybit_coins.append(bybit_control.json()["result"][y]["base_currency"])
         for y in range(len(bybit_coins)):
             if(coins[x]==bybit_coins[y]):
                 counter+=1
-        
-
-#API hiz testi yap mexc yavaş çalışıyor
+        if(counter>1):
+            sortedcoins.append(coins[x])
         print(counter)
+    print(len(coins))
+    print(len(sortedcoins))
         
         
 
-sort(getCoin())
-
+        
+        
+    #coins.sort()
+    
+getCoin()
